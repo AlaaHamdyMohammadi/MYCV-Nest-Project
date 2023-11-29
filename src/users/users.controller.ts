d7aaa86d0087526f93fdc7bce07aba7dd2331990
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-30 01:40:52 +0200

    Create and save a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private userServ: UsersService){}
     @Post('/signup')
     createUser(@Body() body: CreateUserDto){
-        console.log(body);
+        // console.log(body);
+        this.userServ.create(body.email, body.password)
     }
 }
