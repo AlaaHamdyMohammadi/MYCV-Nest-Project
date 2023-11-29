985f027f1a2658685a8cd49c5dab2c9ba0a42d9e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-30 00:48:08 +0200

    Setting Up Validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto){
+        console.log(body);
+    }
+}
