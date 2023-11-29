66252b859dd225a1978edbde68c3be1d7e5308d8
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-30 01:12:35 +0200

    Test Request

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+      // The purpose to make sure that incoming requests don't have extraneous properties in the body that we are not expecting(small security thing)
       whitelist: true,
     }),
   );
