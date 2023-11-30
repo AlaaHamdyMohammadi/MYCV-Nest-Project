2033767b070d06c007ca94024a293414421002e8
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-30 16:38:18 +0200

    Find Method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,26 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
     constructor(private userServ: UsersService){}
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto){
         // console.log(body);
         this.userServ.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string){
+        return this.userServ.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string){
+        return this.userServ.find(email);
     }
 }
