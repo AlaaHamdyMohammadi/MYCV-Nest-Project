a9d739a8c0bde866a541ad97cd3fcb5efe4d3171
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-02 20:40:12 +0200

    Update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,6 +20,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userServ: UsersService) {}
 
@@ -30,7 +31,7 @@ export class UsersController {
   }
 
   //   @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
+
   @Get('/:id')
   findUser(@Param('id') id: string) {
     console.log('handler is running...');
