f34f1c58edb1e74c83d0ec380f8ccc438b6556fe
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-02 22:24:24 +0200

    Implement Signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -7,6 +7,11 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
 
 @Injectable()
 export class AuthService{
@@ -20,6 +25,14 @@ export class AuthService{
         }
 
         // 2. Hash the users password
+            // 1. Generate a salt
+            const salt = randomBytes(8).toString('hex');
+
+            // 2. Hash the salt and the password together
+            const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+            // 3. Join the hashed result and the salt together
+            const result = salt + '.' + hash.toString('hex');
 
         // 3. Create a new user and save it
 
