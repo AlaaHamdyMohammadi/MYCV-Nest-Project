bd3a68645a814853a389890f8cdc60a5b543db27
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-02 21:21:31 +0200

    Implement Signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,10 +1,30 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService{
     constructor(private usersServ: UsersService){}
+
+    async signup(email: string, password: string){
+        // 1. See if email is in use
+        const users = await this.usersServ.find(email);
+        if(users.length){
+            throw new BadRequestException('Email in use');
+        }
+
+        // 2. Hash the users password
+
+        // 3. Create a new user and save it
+
+        // 4. Return the user 
+    }
+
+    signin(){}
 }
