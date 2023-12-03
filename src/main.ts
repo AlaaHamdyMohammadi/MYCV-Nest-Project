179ed1ce60fc1fad5d0a473b46eedbdf4a44c767
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-03 23:51:42 +0200

    Setting up session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['al26aa']
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       // The purpose to make sure that incoming requests don't have extraneous properties in the body that we are not expecting(small security thing)
