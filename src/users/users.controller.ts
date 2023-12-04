e2360d4b9c1f59651a279cbfbf1e7ed722e9a99c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-04 16:33:19 +0200

    Custom Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,8 +11,6 @@ import {
   Query,
   NotFoundException,
   Session,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,6 +18,8 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -39,9 +39,14 @@ export class UsersController {
   //     return session.color;
   //   }
 
+//   @Get('/whoami')
+//   whoAmI(@Session() session: any) {
+//     return this.userServ.findOne(session.userId);
+//   }
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.userServ.findOne(session.userId);
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
